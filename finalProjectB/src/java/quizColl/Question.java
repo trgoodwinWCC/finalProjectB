ce55ecb7ac6f9cc292282481b8208c5b2d46181e
trgoodwinWCC <>
2017-12-11 15:53:37 -0500

    added getters and setters for question bean

---
+++ finalProjectB/src/java/quizColl/Question.java
@@ -0,0 +1,49 @@
+package quizColl;
+
+import java.util.ArrayList;
+
+
+public class Question {
+    
+    private String question;
+    private int questionIndex;
+    private String type;
+    private ArrayList<String> answers = new ArrayList<>();
+    private int correctAnswerIndex;
+
+    public String getQuestion() {
+        return question;
+    }
+    public int getQuestionIndex() {
+        return questionIndex;
+    }
+    public String getType() {
+        return type;
+    }
+    public ArrayList<String> getAnswers() {
+        return answers;
+    }
+    public int getCorrectAnswerIndex() {
+        return correctAnswerIndex;
+    }
+    // end of getters
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+    public void setQuestionIndex(int questionIndex) {
+        this.questionIndex = questionIndex;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public void setCorrectAnswerIndex(int correctAnswerIndex) {
+        this.correctAnswerIndex = correctAnswerIndex;
+    }
+    public void setAnswers(String answer) {
+        if (this.answers.contains(answer))
+            System.out.println("This answer is already added.");
+        else
+            this.answers.add(answer);
+    }
+    
+}
